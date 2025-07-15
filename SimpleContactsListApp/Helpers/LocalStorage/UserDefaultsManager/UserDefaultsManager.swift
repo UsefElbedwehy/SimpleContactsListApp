325c93208a05e4b08b1134a37cc2deb987594979
Usef-Elbedwehy <youssef.elbedwehy@4sale.tech>
2025-07-15 18:56:10 +0300

    add UserDefaultsManager to handle local contacts

---
+++ SimpleContactsListApp/Helpers/LocalStorage/UserDefaultsManager/UserDefaultsManager.swift
@@ -0,0 +1,35 @@
+//
+//  UserDefaultsManager.swift
+//  SimpleContactsListApp
+//
+//  Created by Usef on 15/07/2025.
+//
+
+import Foundation
+
+final class UserDefaultsManager {
+    static let shared = UserDefaultsManager()
+    private let key = "contacts"
+    
+    private init() {}
+    
+    func saveContacts(_ contacts: [Contact]) {
+        if let encodedData = try? JSONEncoder().encode(contacts) {
+            UserDefaults.standard.set(encodedData, forKey: key)
+        }
+    }
+    
+    func loadContacts() -> [Contact] {
+        if let decodedData = UserDefaults.standard.data(forKey: key),
+           let contacts = try? JSONDecoder().decode([Contact].self, from: decodedData) {
+            return contacts
+        }
+        return []
+    }
+    
+    func saveContact(_ contact: Contact) {
+        var contacts = loadContacts()
+        contacts.append(contact)
+        saveContacts(contacts)
+    }
+}
