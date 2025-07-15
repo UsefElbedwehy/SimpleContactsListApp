8c1d0711fa62e94b66b77c0825e4d04f67a33480
Usef-Elbedwehy <youssef.elbedwehy@4sale.tech>
2025-07-15 18:47:06 +0300

    add model which is shared allover the views cuz all of them use it

---
+++ SimpleContactsListApp/Shared/Model/Contact/ContactModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContactModel.swift
+//  SimpleContactsListApp
+//
+//  Created by Usef on 15/07/2025.
+//
+
+import Foundation
+
+struct Contact {
+    let name: String
+    let phone: String
+}
